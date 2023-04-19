8db398e4c7815c5e765cfee2bf1cb0ceb5771fee
jimmy094 <jimmyalvarezjr@gmail.com>
2023-04-18 21:19:00 -0400

    init the project

---
+++ server/src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+import { userRouter } from './routes/users.js';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter)
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    app.listen(3001, () => console.log(`Running on Server Port: 3001 👻 `));
+})
+.catch((error) => console.log(`${error} did not connect`));
+
