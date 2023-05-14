ffb08340198b543898b7bc2a0f39b903747e58dd
jimmy094 <jimmyalvarezjr@gmail.com>
2023-05-14 14:51:16 -0400

    finished create page

--- client/watches/src/pages/createWatch.jsx
+++ client/watches/src/pages/createWatch.jsx
@@ -1,30 +1,51 @@
 import { useState } from "react";
 import axios from 'axios'
+import { useGetUserID } from "../hooks/useGetUserID";
+import { useNavigate } from 'react-router-dom'
 
 export const CreateWatch = () => {
+    const userID = useGetUserID();
+    
     const [ watch, setWatch ] = useState({
         brand: "",
         model:"",
         year: 0,
-        description: "",
+        description: [],
         imageUrl: "",
         price: 0,
+        userOwner: userID,
     });
+
+const navigate = useNavigate();   
+
 //grab target input, changing name to new value
     const handleChange = (event) => {
         const {name, value} = event.target;
         setWatch({...watch, [name]: value})
     };
 
-    const addDesc = () => {
-        setWatch({...watch, description: [...watch.description, ""] })
+// grab value from target, make copy of description, change idx of copy to idx received from function. Use spread operator to list watch desc and add new desc element to that using new descriptions idx on Line 31
+    const handleDescChange = (event, idx) => {
+        const { value } = event.target;
+        const description = watch.description;
+        description[idx] = value;
+        setWatch({ ...watch, description })
+    };
+//list values of descriptions plus empty string by default. Then change value to what is entered.
+    const handleAddDesc = () => {
+        const description = [...watch.description, ""]
+        setWatch({...watch, description })
     }
 
-    const onSubmit = async (event) => {
+console.log(watch)
+
+//after submitting watch useNavigate back to the "/" home page
+    const handleSubmit = async (event) => {
         event.preventDefault();
         try {
             await axios.post("http://localhost:3001/watches", watch);
-            alert("Watch created!")
+            alert("Watch created!");
+            navigate("/")
         } catch (error) {
             console.log(error)
         }
@@ -35,30 +56,38 @@ export const CreateWatch = () => {
     return (
     <div className="create-watch">
 
-        <h2>Create your Watch</h2>
+        <h2>Create your Watch</h2> 
 
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="brand">Brand</label>
             <input type="text" id="brand" name="brand" onChange={handleChange}/>
 
             <label htmlFor="model">Model</label>
-            <input type="text" id="model" name="model" />
+            <input type="text" id="model" name="model" onChange={handleChange}/>
 
             <label htmlFor="year">Year</label>
-            <input type="text" id="year" name="year" />
+            <input type="text" id="year" name="year" onChange={handleChange}/>
 
             <label htmlFor="description">Description</label>
-            <textarea id="description" name="description" onChange={handleChange}></textarea>
-
-            <button onClick={addDesc} type="button">Add description</button>
+          
+                {watch.description.map((description, idx) => (
+                    <input 
+                    key={idx} 
+                    type="text" 
+                    name="description" 
+                    value={description}
+                    onChange={(event) => handleDescChange(event, idx)}
+                    />
+                ))}
+            <button onClick={handleAddDesc} type="button">Add description</button>
 
             <label htmlFor="imageUrl">Image Url</label>
             <input type="text" id="imageUrl" name="imageUrl" onChange={handleChange} />
 
             <label htmlFor="price">Price</label>
-            <input type="text" id="year" name="year" />
+            <input type="text" id="price" name="price" onChange={handleChange}/>
 
-            <button onSubmit={onSubmit} type="submit">Create your watch</button>
+            <button onSubmit={handleSubmit} type="submit">Create watch</button>
         </form>
     
     
