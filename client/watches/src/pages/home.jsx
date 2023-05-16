30ff3de3fde77264a28c94028fb20bf6a283e99b
jimmy094 <jimmyalvarezjr@gmail.com>
2023-05-16 00:10:55 -0400

    saveWatch function some css

--- client/watches/src/pages/home.jsx
+++ client/watches/src/pages/home.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 import axios from "axios";
+import {useGetUserID} from "../hooks/useGetUserID";
+
 
 
 export const Home = () => {
     const [watches, setWatches] = useState([]);
+    const userID = useGetUserID();
 
     useEffect(() => {
     const fetchWatch = async () => {
@@ -20,8 +23,13 @@ export const Home = () => {
     }, []);
 
     const saveWatch = async (watchID) => {
-
-    }
+        try {
+            const response = await axios.put("http://localhost:3001/watches", {watchID, userID});
+            console.log(response)
+        } catch (error) {
+            console.log(error)
+        }
+    };
 
     return( 
     <div>
@@ -31,7 +39,7 @@ export const Home = () => {
                 <li key={watch._id}>
                     <div>
                         <h2>{watch.brand}</h2>
-                        <button onClick={() => saveWatch(watch._id)}>Save</button>
+                        <button className='saveBtn' onClick={() => saveWatch(watch._id)}>Save</button>
                         <div classname="instructions">
                             <p>Description: {watch.description} </p>
                             <p>Avg price: ${watch.price} </p>
