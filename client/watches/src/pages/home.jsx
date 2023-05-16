d7c3a31299eb81229d6115fd4d6cda0223c45d50
jimmy094 <jimmyalvarezjr@gmail.com>
2023-05-15 20:45:48 -0400

    home page and rendering

--- client/watches/src/pages/home.jsx
+++ client/watches/src/pages/home.jsx
@@ -1,3 +1,46 @@
+import { useEffect, useState } from 'react';
+import axios from "axios";
+
+
 export const Home = () => {
-    return <div>Home</div>
-};
+    const [watches, setWatches] = useState([]);
+
+    useEffect(() => {
+    const fetchWatch = async () => {
+        try {
+            const response = await axios.get("http://localhost:3001/watches");
+            setWatches(response.data)
+            console.log(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+        
+        } 
+        fetchWatch();
+    }, []);
+
+    const saveWatch = async (watchID) => {
+
+    }
+
+    return( 
+    <div>
+        <h2>Watches</h2>
+        <ul>
+            {watches.map((watch) => (
+                <li key={watch._id}>
+                    <div>
+                        <h2>{watch.brand}</h2>
+                        <button onClick={() => saveWatch(watch._id)}>Save</button>
+                        <div classname="instructions">
+                            <p>Description: {watch.description} </p>
+                            <p>Avg price: ${watch.price} </p>
+                            <p>Year: {watch.year}</p>
+                        </div>
+                        <img src={watch.imageUrl} alt={watch.name} />
+                    </div>
+                </li>
+            ))}
+        </ul>
+    </div>
+)};
